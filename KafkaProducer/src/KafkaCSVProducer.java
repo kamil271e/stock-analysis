1c5abecb5e141abdb815eeaa6c368d55051ff0c2
kamil <kamil271e@gmail.com>
2024-06-01 19:08:41 +0200

    Kafka producer + guide

---
+++ KafkaProducer/src/KafkaCSVProducer.java
@@ -0,0 +1,58 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.io.File;
+import java.util.Arrays;
+
+public class KafkaCSVProducer {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String inputDir = args[0];
+        String topic = args[1];
+        String header = args[2];
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        //String[] csvFiles = new String[100];
+
+        final File folder = new File(inputDir);
+        File[] listOfFiles = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".csv"));
+        String[] listOfPaths = Arrays.stream(listOfFiles)
+                .map(File::getAbsolutePath)
+                .toArray(String[]::new);
+        Arrays.sort(listOfPaths);
+        System.out.println(listOfPaths);
+
+        // Process each CSV file
+        for (String csvFile : listOfPaths) {
+            try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+                String line;
+                for(int i = 0; i <= Integer.parseInt(header); i++){
+                    br.readLine();
+                }
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                    // Send CSV line to Kafka topic
+                    producer.send(new ProducerRecord<>(topic, line));
+                    Thread.sleep(1000); // Wait for 1 second before sending next line
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Close Kafka Producer
+        producer.close();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # stock-analysis
 Stock data anomaly detection: Apache Kafka Streams
+
+## KafkaProducer guide
+
+Copy data from bucket to HDFS
+```sh
+hadoop fs -mkdir -p /user/kamil271e
+hadoop fs -mkdir /user/kamil271e/stocks_result
+mkdir -p ~/gcs_data/stocks_result
+gsutil cp -r gs://pbd-24-kk/stocks_result ~/gcs_data/
+hadoop fs -put ~/gcs_data/stocks_result/* /user/kamil271e/stocks_result/
+```
+
+List data to be sure we good to go:
+```sh
+hadoop fs -ls /user/kamil271e/stocks_result
+```
+
+
+Start kafka topic:
+```sh
+kafka-topics.sh --create \
+--bootstrap-server ${CLUSTER_NAME}-w-1:9092 \
+--replication-factor 2 --partitions 3 --topic stocks-topic
+```
+
+Run Kafka producer (for now it works on local fs; there was some issues with hdfs):
+```sh
+java -cp "/usr/lib/kafka/libs/*:KafkaCSVProducer.jar" KafkaCSVProducer gcs_data/stocks_result stocks-topic 1
+```
+
+Troubleshooting TODO:
+
+I've tried to obtain nodename and port with this commands:
+
+```sh
+hdfs getconf -confKey fs.defaultFS
+hdfs getconf -confKey dfs.namenode.http-address
+```
+
+and then run KafkaProducer using HDFS files:
+```sh
+java -cp "/usr/lib/kafka/libs/*:KafkaCSVProducer.jar" KafkaCSVProducer hdfs://pbd-cluster-m:9870/user/kamil271e/stocks_result stocks-topic 1
+```
+but files were not loaded properly:C
